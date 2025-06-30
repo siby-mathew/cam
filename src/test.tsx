b9e1dc697629cbf53c25c111e2fe6647ff2feac6
Siby Mathew <siby@sonicdex.io>
2025-06-30 18:17:01 +0530

    dfx

--- src/test.tsx
+++ src/test.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const CameraCapture = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
